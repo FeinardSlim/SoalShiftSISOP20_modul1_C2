0f215d93d7d9f493f45953fcea4af7407d4b6e21
FeinardSlim <58687783+FeinardSlim@users.noreply.github.com>
2020-02-21 09:40:16 +0700

    Add files via upload

--- praktikum3.sh
+++ praktikum3.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #crontab-e 05 6/8 * * 0-5 praktikum3.sh >> wget.log
-iter=0
-while [[ $iter -ne 28 ]]
+iter=1
+while [[ $iter -ne 29 ]]
 do
   sort_name="pdkt_Kusuma"
   i=1
   c="${sort_name}_${i}"
-  if [[ -e $c || -L $c ]]
+  if [[ -e $c ]]
     then
-      while [[ -e $c || -L $c ]] ;
+      while [[ -e $c ]] ;
         do
           let  i++;
           c="${sort_name}_${i}"
@@ -17,7 +17,7 @@ do
   else
     name=pdkt_Kusuma_1
   fi
-  echo "TEST"
+  echo "DOWNLOADING : $iter"
   wget -O $name https://loremflickr.com/320/240/cat -o temp.log
   let iteration++
   grep -r "Location" temp.log >> wget.log
